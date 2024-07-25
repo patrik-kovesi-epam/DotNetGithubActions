f8838d0f8f626a56d22f1dd7d222f8f8195701a1
Patrik_Kovesi <patrik_kovesi@epam.com>
2024-07-25 17:36:05 +0200

    Initial commit

---
+++ tests/MyProject.ApiTests/ApiTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyProject.ApiTests
+{
+    public class ApiTests
+    {
+        private readonly HttpClient _client = new HttpClient();
+
+        [Test]
+        public async Task GetWeatherForecast_ReturnsSuccessStatusCode()
+        {
+            var response = await _client.GetAsync("http://localhost:5000/weatherforecast");
+            response.EnsureSuccessStatusCode();
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/MyProject.UiTests/UiTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace MyProject.UiTests
+{
+    public class UiTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+                var screenshotPath = Path.Combine(TestContext.CurrentContext.WorkDirectory, "screenshots");
+                Directory.CreateDirectory(screenshotPath);
+                var screenshotFile = Path.Combine(screenshotPath, $"{TestContext.CurrentContext.Test.Name}.png");
+                screenshot.SaveAsFile(screenshotFile);
+            }
+            _driver.Quit();
+        }
+
+        [Test]
+        public void ExampleTest()
+        {
+            _driver.Navigate().GoToUrl("https://example.com");
+            Assert.AreEqual("Example Domain", _driver.Title);
+        }
+    }
+}
